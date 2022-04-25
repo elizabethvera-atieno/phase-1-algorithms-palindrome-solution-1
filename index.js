ce2bbd7eafb656ebd01d700cebde5fa8286c3fcb
elizabethvera-atieno <atienolizvera@gmail.com>
2022-04-25 10:01:57 +0300

    complete test

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // reverse the input string
+    const reversedWord = reverseString(word);
+    // compare the reversed string to the input
+    return word === reversedWord;
+  
 }
 
-/* 
-  Add your pseudocode here
-*/
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 
 /*
-  Add written explanation of your solution here
+ Split the string
+That takes up more memory to store the new array, as well as the time it takes JavaScript to iterate over each character of the string to produce a new array
+Reverse the array
+That takes up more memory to store the reversed array, as well as the time it takes JavaScript to iterate over the array to produce a reversed array
+Join the string
+That takes up more memory to store the new string, as well as the time it takes JavaScript to iterate over the array to produce a string
 */
 
 // You can run `node index.js` to view these console logs
